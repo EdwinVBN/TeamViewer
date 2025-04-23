0feb3be982820d03d9efa740d2805d49a7e4d937
EdwinVBN <Katuku.edwin@gmail.com>
2025-04-23 15:50:13 +0200

    Screenshot en recording klaar

--- TeamViewer/MainWindow.xaml.cs
+++ TeamViewer/MainWindow.xaml.cs
@@ -1,24 +1,71 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
+using System.Drawing;
+using System.Threading.Tasks;
 
 namespace TeamViewer
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+    
     public partial class MainWindow : Window
     {
+        private bool recording_Btn_Is_Active = false;
+
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        private async void record_Button(object sender, RoutedEventArgs e)
+        {
+            recording_Btn_Is_Active = !recording_Btn_Is_Active;
+            start_Rec_Btn.Content = recording_Btn_Is_Active ? "Stop" : "Start";
+
+            while (recording_Btn_Is_Active)
+            {
+                record_Screen();
+                await Task.Delay(33);
+            }
+        }
+
+        private void record_Screen()
+        {
+            string filename = "Screenshot-" + DateTime.Now.ToString("HHmmss_fff") + ".jpeg";
+
+            int screenLeft = (int)SystemParameters.VirtualScreenLeft;
+            int screenTop = (int)SystemParameters.VirtualScreenTop;
+            int screenWidth = (int)SystemParameters.VirtualScreenWidth;
+            int screenHeight = (int)SystemParameters.VirtualScreenHeight;
+
+            Bitmap bitmap_Screen = new Bitmap(screenWidth, screenHeight);
+            Graphics g = Graphics.FromImage(bitmap_Screen);
+
+            g.CopyFromScreen(screenLeft, screenTop, 0, 0, bitmap_Screen.Size);
+
+            bitmap_Screen.Save("D:\\TeamViewerRepo\\TeamViewer\\video\\" + filename);
+        }
+
+
+        private void screenshot_Button(object sender, RoutedEventArgs e)
+        { 
+            make_Screenshot();
+            MessageBox.Show("screenshot is gemaakt");
+        }
+
+        private void make_Screenshot()
+        {
+            string filename = "Screenshot-" + DateTime.Now.ToString("hhmmss") + ".jpeg";
+
+            int screenLeft = (int) SystemParameters.VirtualScreenLeft;
+            int screenTop = (int) SystemParameters.VirtualScreenTop;
+            int screenWidth = (int) SystemParameters.VirtualScreenWidth;
+            int screenHeight = (int) SystemParameters.VirtualScreenHeight;
+
+            Bitmap bitmap_Screen = new Bitmap(screenWidth, screenHeight);
+            Graphics g = Graphics.FromImage(bitmap_Screen);
+
+            g.CopyFromScreen(screenLeft, screenTop, 0, 0, bitmap_Screen.Size);
+
+            bitmap_Screen.Save("D:\\TeamViewerRepo\\TeamViewer\\images\\" + filename);
+        }
+
     }
 }
