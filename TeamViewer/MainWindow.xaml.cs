2efae4b4b5e77c0807ccc289c6bf3e380c6b1ae3
EdwinVBN <Katuku.edwin@gmail.com>
2025-04-24 21:08:34 +0200

    Recording in class en TCPServer/Listener gemaakt

--- TeamViewer/MainWindow.xaml.cs
+++ TeamViewer/MainWindow.xaml.cs
@@ -7,42 +7,16 @@ namespace TeamViewer
     
     public partial class MainWindow : Window
     {
-        private bool recording_Btn_Is_Active = false;
 
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private async void record_Button(object sender, RoutedEventArgs e)
+        private void rec_Btn_listener(object sender, RoutedEventArgs e)
         {
-            recording_Btn_Is_Active = !recording_Btn_Is_Active;
-            start_Rec_Btn.Content = recording_Btn_Is_Active ? "Stop" : "Start";
-
-            while (recording_Btn_Is_Active)
-            {
-                record_Screen();
-                await Task.Delay(33);
-            }
+            Record.record_Button(sender, e, start_Rec_Btn);
         }
-
-        private void record_Screen()
-        {
-            string filename = "Screenshot-" + DateTime.Now.ToString("HHmmss_fff") + ".jpeg";
-
-            int screenLeft = (int)SystemParameters.VirtualScreenLeft;
-            int screenTop = (int)SystemParameters.VirtualScreenTop;
-            int screenWidth = (int)SystemParameters.VirtualScreenWidth;
-            int screenHeight = (int)SystemParameters.VirtualScreenHeight;
-
-            Bitmap bitmap_Screen = new Bitmap(screenWidth, screenHeight);
-            Graphics g = Graphics.FromImage(bitmap_Screen);
-
-            g.CopyFromScreen(screenLeft, screenTop, 0, 0, bitmap_Screen.Size);
-
-            bitmap_Screen.Save("D:\\TeamViewerRepo\\TeamViewer\\video\\" + filename);
-        }
-
 
         private void screenshot_Button(object sender, RoutedEventArgs e)
         { 
---
+++ TeamViewer/Record.cs
@@ -0,0 +1,38 @@
+﻿using System.Drawing;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace TeamViewer
+{
+    public class Record
+    {
+        private static bool recording_Btn_Is_Active = false;
+        public static async void record_Button(object sender, RoutedEventArgs e, Button btn)
+        {
+            recording_Btn_Is_Active = !recording_Btn_Is_Active;
+            btn.Content = recording_Btn_Is_Active ? "Stop" : "Start";
+            while (recording_Btn_Is_Active)
+            {
+               record_Screen();
+               await Task.Delay(33);
+            }
+        }
+
+        private static void record_Screen()
+        {
+            string filename = "Screenshot-" + DateTime.Now.ToString("HHmmss_fff") + ".jpeg";
+
+            int screenLeft = (int)SystemParameters.VirtualScreenLeft;
+            int screenTop = (int)SystemParameters.VirtualScreenTop;
+            int screenWidth = (int)SystemParameters.VirtualScreenWidth;
+            int screenHeight = (int)SystemParameters.VirtualScreenHeight;
+
+            Bitmap bitmap_Screen = new Bitmap(screenWidth, screenHeight);
+            Graphics g = Graphics.FromImage(bitmap_Screen);
+
+            g.CopyFromScreen(screenLeft, screenTop, 0, 0, bitmap_Screen.Size);
+
+            bitmap_Screen.Save("D:\\TeamViewerRepo\\TeamViewer\\video\\" + filename);
+        }
+    }
+}
